64207af000d74cc8373999f6bb6f97ce578d1ce6
Dixit-Anmol <bholudixitanmoldixit@gmail.com>
2025-04-13 13:07:51 +0530

    POJO class

--- src/main/java/org/example/movieshub/movie/model/Movie.java
+++ src/main/java/org/example/movieshub/movie/model/Movie.java
@@ -1,4 +1,13 @@
-package org.example.movieshub.movie;
+package org.example.movieshub.movie.model;
 
 public class Movie {
+    private long id;
+    private String title;
+    private String year;
+    private String genre;
+    private String description;
+    private double rating;
+    private String ImageUrl;
+
 }
+// pojo class - blank const, parameter const , getter setter , equals and hashcode , to string
