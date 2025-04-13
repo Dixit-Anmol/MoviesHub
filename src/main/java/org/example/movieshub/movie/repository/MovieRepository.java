4243c8c0bcb8800dcdf195ae863cebf5838fc9b6
Dixit-Anmol <bholudixitanmoldixit@gmail.com>
2025-04-13 13:00:34 +0530

    repos

--- src/main/java/org/example/movieshub/movie/repository/MovieRepository.java
+++ src/main/java/org/example/movieshub/movie/repository/MovieRepository.java
@@ -1,4 +1,7 @@
 package org.example.movieshub.movie.repository;
 
-public interface MovieRepository {
+import org.example.movieshub.movie.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
